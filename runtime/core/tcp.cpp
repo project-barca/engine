8d5edbaef021269a49f7228b8596845a2d5b9b3b
ahsouza <annibalhsouza@gmail.com>
2022-03-28 18:28:29 -0300

    feat: bind a link to socket

--- runtime/core/tcp.cpp
+++ runtime/core/tcp.cpp
@@ -17,7 +17,13 @@ int main() {
   hint.sin_family = AF_INET;
   //assign hosts for small networks
   hint.sin_port = htons(54000);
-  inet_pton(); // define loopback localhost 127.0.0.1
-
+  // set version 4 network address with any available ip to a link
+  inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr);
+  // check if the info link is bound to the socket
+  if (bind(listening, AF_INET, &hint, sizeof(hint)) == -1) {
+    cerr << "Não foi possível vincular ao IP/port";
+    return -2;
+  }
+  
   return 0;
 }
