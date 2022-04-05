22643a0811580759d9e7a153b7d4f54dd29b8a9f
ahsouza <annibalhsouza@gmail.com>
2022-04-05 20:13:14 -0300

    feat: add methods and checks on server connection

--- runtime/core/win/tcp_server.cpp
+++ runtime/core/win/tcp_server.cpp
@@ -23,6 +23,7 @@ void main() {
   // check if socket is valid
   if (sock == INVALID_SOCKET) {
     cerr << "Não foi possível criar o socket" << WSAGetLastError() << endl;
+    WSACleanup();
     return;
   }
   // hint structure
@@ -30,6 +31,13 @@ void main() {
   hint.sin_family == AF_INET;
   hint.sin_port = htons(port);
   inet_pton(AF_INET, ipAddress.c_str(), &hint.sin_addr);
-
+  // connect to server
+  int connRes = connect(sock, (sockaddr*)&hint, sizeof(hint));
+  if (connResult == SOCKET_ERROR) {
+    cerr << "Não foi possível conectar ao servidor" << WSAGetLastError() << endl;
+    closesocket(sock);
+    WSACleanup();
+    return;
+  }
 
 }
