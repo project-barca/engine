a844dcc67ffb2cb13b243d6db34be9fef0d4d292
ahsouza <annibalhsouza@gmail.com>
2022-04-04 22:27:57 -0300

    feat: add tcp server to winsock

---
+++ runtime/core/win/tcp_server.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+using namespace std;
+
+void main() {
+  // IP Address of the server
+  string ipAddr = "127.0.0.1"
+  // listening port on the server  
+  int port = 54000;
+}
