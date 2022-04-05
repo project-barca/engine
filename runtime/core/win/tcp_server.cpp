6b615f473b7d089efdfe2c5d82eef432062e8bbc
ahsouza <annibalhsouza@gmail.com>
2022-04-05 19:02:37 -0300

    feat: add methods of creating socket starting winsock on windows

--- runtime/core/win/tcp_server.cpp
+++ runtime/core/win/tcp_server.cpp
@@ -7,7 +7,22 @@ using namespace std;
 
 void main() {
   // IP Address of the server
-  string ipAddr = "127.0.0.1"
+  string ipAddr = "127.0.0.1";
   // listening port on the server  
   int port = 54000;
+  // initialize winSock
+  WSAData data;
+  WORD vr = MAKEWORD(2, 2);
+  int wsResult = WSAStartup(vr, &data);
+  if (wsResult != 0) {
+    cerr << "Não foi possível iniciar o Winsock" << wsResult << endl;
+    return;
+  }
+  // create a socket
+  SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+  // check if socket is valid
+  if (sock == INVALID_SOCKET) {
+    cerr << "Não foi possível criar o socket" << WSAGetLastError() << endl;
+    return;
+  }
 }
