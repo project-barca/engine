b90cce2464e113cae2dbf0aafdc150b2da419d4d
ahsouza <annibalhsouza@gmail.com>
2022-04-05 21:44:34 -0300

    feat: implement network address binding for socket

--- runtime/core/win/tcp_server.cpp
+++ runtime/core/win/tcp_server.cpp
@@ -14,4 +14,18 @@ void main() {
   if (WSOK != 0) {
     cerr << "Não foi possível inicializar o Winsock" << endl;
   }
+  // create a socket
+  SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
+  if (listening == INVALID_SOCKET) {
+    cerr << "Não foi possível criar um socket" << endl;
+    return;
+  }
+  // bind the ip address and port to a socket
+  sockaddr_in hint;
+  hint.sin_family = AF_INET;
+  hint.sin_port = htons(54000);
+  hint.sin_addr.S_un.S_addr = INADDR_ANY;
+
+  bind(listening, (sockaddr*)&hint, sizeof(hint));
+
 }
