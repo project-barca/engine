de863fbc761e3c8098986bdd7a725bdae8ae874f
ahsouza <annibalhsouza@gmail.com>
2022-05-02 18:40:03 -0300

    feat: add methods to generate logs

---
+++ include/monitor.h
@@ -0,0 +1,72 @@
+#ifndef PROJ_H
+#define PROJ_H
+#include <windows.h>
+#include <iostream>
+#include "directory.h"
+#include <future>
+
+class Monitor {
+  public:
+    // generate logs over server
+    int logsServer(char* dir, int (*func)(std::string, std::string)) {
+      const char* newDir = R"(C:\\Barca\\1.0\\data)";
+      Directory directory;
+      std::cout << directory.setCurrentPath(newDir) << '\n';
+      std::future<int> setServerLogs = std::async (
+        func, "./barca.server.log",
+        "{}");
+      setServerLogs.get();
+
+      return EXIT_SUCCESS;
+    };
+    // generate logs over activity
+    int logsActivity(char* dir, int (*func)(std::string, std::string)) {
+      const char* newDir = R"(C:\\Barca\\1.0\\log)";
+      Directory directory;
+      std::cout << directory.setCurrentPath(newDir) << '\n';
+      std::future<int> setActivityLogs = std::async (
+        func, "./barca.activity.log",
+        "{}");
+      setActivityLogs.get();
+
+      return EXIT_SUCCESS;
+    };
+    // generate logs over firewall
+    int logsFirewall(char* dir, int (*func)(std::string, std::string)) {
+      const char* newDir = R"(C:\\Barca\\1.0\\log)";
+      Directory directory;
+      std::cout << directory.setCurrentPath(newDir) << '\n';
+      std::future<int> setFirewallLogs = std::async (
+        func, "./barca.firewall.log",
+        "{}");
+      setBackUp.get();
+
+      return EXIT_SUCCESS;
+    };
+    // generate logs over firewall
+    int logsSecurity(char* dir, int (*func)(std::string, std::string)) {
+      const char* newDir = R"(C:\\Barca\\1.0\\log)";
+      Directory directory;
+      std::cout << directory.setCurrentPath(newDir) << '\n';
+      std::future<int> setSecurityLogs = std::async (
+        func, "./barca.backup.log",
+        "{}");
+      setBackUp.get();
+
+      return EXIT_SUCCESS;
+    };
+    // generate logs
+    int logs(char* dir, int (*func)(std::string, std::string)) {
+      const char* newDir = R"(C:\\Barca\\1.0\\log)";
+      Directory directory;
+      std::cout << directory.setCurrentPath(newDir) << '\n';
+      std::future<int> setBackUp = std::async (
+        func, "./barca.log", 
+        "{}");
+      setBackUp.get();
+
+      return EXIT_SUCCESS;
+    };
+};
+
+#endif
--- include/proj.h
+++ include/proj.h
@@ -12,7 +12,7 @@ class Proj {
       const char* newDir = R"(C:\\Barca\\1.0\\data)";
       Directory directory;
       std::cout << directory.setCurrentPath(newDir) << '\n';
-      // call the function setBackUp asynchronously to check whether the number is prime or not:
+      // call the function setBackUp asynchronously
       std::future<int> setBackUp = std::async (
         func, "./proj.backup.bat", 
         "@echo off\n\nrem for each file in your folder\nfor %%a in (\".\\*\") do (\n  rem check if the file has an extension and if it is not our script\n  if \"%%~xa\" NEQ \"\" if \"%%~dpxa\" NEQ \"%~dpx0\" (\n    rem check if extension folder exists, if not it is created\n    if not exist \"%%~xa\" mkdir \"%%~xa\"\n    rem move the file to directory\n    move \"%%a\" \"%%~dpa%%~xa\"\n  )\n)");
