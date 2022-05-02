6e7cc03471eae38917aebea705ea8c7654ef6af1
ahsouza <annibalhsouza@gmail.com>
2022-05-02 11:58:38 -0300

    refactor: add script backup methods for windows

--- include/proj.h
+++ include/proj.h
@@ -2,39 +2,21 @@
 #define PROJ_H
 #include <windows.h>
 #include <iostream>
-// #include "file.h"
 #include "directory.h"
 #include <future>
 
 class Proj {
   public:
-    // ORGANIZE FILES IN SPECIFIC PROJECT FOLDER
-    int organizeFiles(char* dir, int (*func)(std::string, std::string)) {
-      const char* ex = R"(C:\\Barca)";
+    // backUp your projects, files and folders
+    int backUp(char* dir, int (*func)(std::string, std::string)) {
+      const char* newDir = R"(C:\\Barca\\1.0\\data)";
       Directory directory;
-      // std::cout << directory.getCurrentPath() << '\n';
-      std::cout << directory.setCurrentPath(ex) << '\n';
-      
-      // call the function fnprime() asynchronously to check whether the number is prime or not:
-      // std::future<string> setDir = std::async (directory.setCurrentPath, ex);
-      // setDir.get();
-      std::string s = directory.setCurrentPath(ex);
-      char ch = './barca.proj.bat';
-
-      s.push_back(ch);
-      std::cout << s;
-
-
-      std::cout << "Organizando arquivos. . .\n";
-    
-          
-      // concatenates
-      // strcat(dir,"\\barca.proj.bat");
-      
-
-      func("./barca.proj.bat", "@echo off\n\ncd \"C:\\Program Files\\Microsoft\"\nstart barca.exe");
-
-
+      std::cout << directory.setCurrentPath(newDir) << '\n';
+      // call the function setBackUp asynchronously to check whether the number is prime or not:
+      std::future<int> setBackUp = std::async (
+        func, "./proj.backup.bat", 
+        "@echo off\n\nrem For each file in your folder\nif \"%%~xa\" NEQ \"\" if \"%%~dpxa\" NEQ \"%~dpx0\" (");
+      setBackUp.get();
 
       return EXIT_SUCCESS;
     };
