bbf54b99eb237768647ead9f9bd4f292ceecf4bf
ahsouza <annibalhsouza@gmail.com>
2022-05-03 07:55:22 -0300

    refactor: redo file from header to file

--- include/file.h
+++ include/file.h
@@ -1,5 +1,5 @@
-#ifndef FILE_H
-#define FILE_H
+#ifndef NEWFILE_H
+#define NEWFILE_H
 
 #include <iostream>
 #include <fstream>
@@ -9,24 +9,20 @@ using std::cout; using std::ofstream;
 using std::endl; using std::string;
 using std::cerr; using std::fstream;
 
-class File {
-  public:
-    // generate new file
-    int newFile(const std::string fileName, const std::string content) {
-      string text(content);
-      string filename2(fileName);
-      fstream outfile;
+int newFile(const std::string fileName, const std::string content) {
+  string text(content);
+  string filename2(fileName);
+  fstream outfile;
 
-      outfile.open(filename2, std::ios_base::app);
-      if (!outfile.is_open()) {
-        cerr << "Falha ao tentar abrir o arquivo" << filename2 << '\n';
-      } else {
-        outfile.write(text.data(), text.size());
-        cerr << "Arquivo adicionado!" << endl;
-      }
+  outfile.open(filename2, std::ios_base::app);
+  if (!outfile.is_open()) {
+    cerr << "Falha ao tentar abrir o arquivo" << filename2 << '\n';
+  } else {
+    outfile.write(text.data(), text.size());
+    cerr << "Arquivo adicionado!" << endl;
+  }
 
-      return EXIT_SUCCESS;
-    }
-};
+  return EXIT_SUCCESS;
+}
 
 #endif
