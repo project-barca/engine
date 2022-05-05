4a85cdab3a2c8abd87d516c500604b5db70b1d66
ahsouza <annibalhsouza@gmail.com>
2022-05-04 23:13:47 -0300

    feat: add method to read a file

--- include/file.h
+++ include/file.h
@@ -4,11 +4,13 @@
 #include <iostream>
 #include <fstream>
 #include <Windows.h>
+#include <string>
 
 using std::cout; using std::ofstream;
 using std::endl; using std::string;
 using std::cerr; using std::fstream;
 
+// create a file with content
 int newFile(const std::string fileName, const std::string content) {
   string text(content);
   string filename2(fileName);
@@ -24,7 +26,7 @@ int newFile(const std::string fileName, const std::string content) {
 
   return EXIT_SUCCESS;
 }
-
+// remove a file
 int rmFile(const std::string fileName) {
   const char* fil = fileName.c_str();
   int fileDeleted = remove(fil);
@@ -34,7 +36,21 @@ int rmFile(const std::string fileName) {
     cerr << "Arquivo não encontrado";
     return -1;
   }
-  return 0;
+  return EXIT_SUCCESS;
+}
+// read a file
+int rdFile(const std::string fileName) {
+  const char* fil = fileName.c_str();
+  std::string line;
+  std::ifstream file(fil);
+  if(file.is_open()) {
+    while(getline(file, line)) {
+      std::cout << line << '\n';
+    }
+    file.close();
+  }
+  else std::cout << "Não foi possível encontrar o arquivo: " << fileName;
+  return EXIT_SUCCESS;
 }
 
 #endif
